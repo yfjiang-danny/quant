a0e4765f02a66b27f5bf74111754543ae22e1b4c
Danny <55547441+yfjiang-danny@users.noreply.github.com>
2024-01-09 20:08:56 +0800

    Delete packages/ts/dist directory

--- packages/ts/dist/utils/sleep.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sleep = void 0;
-function sleep(milliseconds) {
-    return new Promise(function (resolve) { return setTimeout(resolve, milliseconds); });
-}
-exports.sleep = sleep;
